02873ff58f655539c2ea1e455400b4ec8d222313
LeCaToX <70011797+LeCaToX@users.noreply.github.com>
2021-09-16 11:59:24 +0700

    lmfao

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-const dotenv = require('dotenv');
-const path = require('path');
-const { SlashCreator, GatewayServer } = require('slash-create');
-const { Client } = require('discord.js');
-const { Player } = require('discord-player');
-const { registerPlayerEvents } = require('./events');
-const { generateDocs } = require('./docs');
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`)
+);
+
+/* ----------------------------------- */
+const dotenv = require("dotenv");
+const path = require("path");
+const { SlashCreator, GatewayServer } = require("slash-create");
+const { Client } = require("discord.js");
+const { Player } = require("discord-player");
+const { registerPlayerEvents } = require("./events");
+const { generateDocs } = require("./docs");
 
 dotenv.config();
 
 const client = new Client({
-    intents: [
-        'GUILDS',
-        'GUILD_VOICE_STATES'
-    ]
+    intents: ["GUILDS", "GUILD_VOICE_STATES"],
 });
 
 client.player = new Player(client);
 registerPlayerEvents(client.player);
 
 const creator = new SlashCreator({
-  applicationID: process.env.DISCORD_CLIENT_ID,
-  token: process.env.DISCORD_CLIENT_TOKEN,
+    applicationID: process.env.DISCORD_CLIENT_ID,
+    token: process.env.DISCORD_CLIENT_TOKEN,
 });
 
-client.on('ready', () => {
+client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
-    console.log('Generating docs...');
+    console.log("Generating docs...");
     generateDocs(creator.commands);
 });
 
 creator
     .withServer(
-        new GatewayServer(
-            (handler) => client.ws.on('INTERACTION_CREATE', handler)
+        new GatewayServer((handler) =>
+            client.ws.on("INTERACTION_CREATE", handler)
         )
     )
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+    .registerCommandsIn(path.join(__dirname, "commands"));
 
-if (process.env.DISCORD_GUILD_ID) creator.syncCommandsIn(process.env.DISCORD_GUILD_ID);
+if (process.env.DISCORD_GUILD_ID)
+    creator.syncCommandsIn(process.env.DISCORD_GUILD_ID);
 else creator.syncCommands();
 
 client.login(process.env.DISCORD_CLIENT_TOKEN);
